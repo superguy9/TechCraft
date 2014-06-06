2e1c8b49699ee7a9ea4b4b694d1c2395d6ffb360
superguy9 <superguy2468@gmail.com>
2014-06-05 18:13:30 -0700

    added main class "shell"

---
+++ java/me/superguy9/techcraft/TechCraft.java
@@ -0,0 +1,22 @@
+package me.superguy9.techcraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ModInformation.MODID, name = ModInformation.NAME, version = ModInformation.VERSION)
+public class TechCraft {
+	
+	@Instance(ModInformation.MODID)
+	public static TechCraft instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
