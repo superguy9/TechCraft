26de639a26540c1086f2e836dcb14a32f3b9f0b0
superguy9 <superguy2468@gmail.com>
2014-06-05 18:14:07 -0700

    added ModInformation class for storing values for the mod

---
+++ java/me/superguy9/techcraft/ModInformation.java
@@ -0,0 +1,8 @@
+package me.superguy9.techcraft;
+
+public class ModInformation {
+
+	public static final String MODID = "TechCraft";
+	public static final String NAME = "TechCraft";
+	public static final String VERSION = "Alpha v1.0";
+}
