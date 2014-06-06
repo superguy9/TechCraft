27b791f29aa54a1f5b6f036e29e5e6c11010932f
superguy9 <superguy2468@gmail.com>
2014-06-05 18:42:04 -0700

    updated class

--- java/me/superguy9/techcraft/TechCraft.java
+++ java/me/superguy9/techcraft/TechCraft.java
@@ -1,8 +1,10 @@
 package me.superguy9.techcraft;
 
+import me.superguy9.techcraft.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -13,8 +15,13 @@ public class TechCraft {
 	@Instance(ModInformation.MODID)
 	public static TechCraft instance;
 	
+	@SidedProxy(clientSide = "me.superguy9.techcraft.proxy.ClientProxy", serverSide = "me.superguy9.techcraft.proxy.CommonProxy")
+	public static CommonProxy proxy;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		proxy.initRenderers();
+		proxy.initSounds();
 	}
 	
 	@EventHandler
